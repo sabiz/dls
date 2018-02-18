f197758823daf2bd9b8afc42cbd9c2a6aea26651
AkiraOnoda <Akira.Onoda-N145@nid.co.jp>
2018-02-19 07:57:56 +0900

    Add scraper

--- SummoScraper/scraping.py
+++ SummoScraper/scraping.py
@@ -1,36 +1,33 @@
 # Thanks http://www.analyze-world.com/entry/2017/10/09/062445
 from bs4 import BeautifulSoup
 import requests
-import pandas as pd
-from pandas import Series, DataFrame
 import time
 from tqdm import tqdm
 import re
+import csv
+import random
 
 
 url_list = []
-# 東京都都心部
-url_of_center = 'https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=13&sc=13101&sc=13102&sc=13103&sc=13104&sc=13105&sc=13113&cb=0.0&ct=9999999&mb=0&mt=9999999&et=9999999&cn=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sngz=&po1=12&pc=50'
-# 東京都南部
-url_of_south = 'https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=13&sc=13109&sc=13110&sc=13111&sc=13112&cb=0.0&ct=9999999&mb=0&mt=9999999&et=9999999&cn=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sngz=&po1=12&pc=50'
-# 東京都西部
-url_of_west = 'https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=13&sc=13114&sc=13115&sc=13120&cb=0.0&ct=9999999&mb=0&mt=9999999&et=9999999&cn=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sngz=&po1=12&pc=50'
-# 東京都北部
-url_of_north = 'https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=13&sc=13116&sc=13117&sc=13119&cb=0.0&ct=9999999&mb=0&mt=9999999&et=9999999&cn=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sngz=&po1=12&pc=50'
-# 東京都東部
-url_of_east = 'https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=13&sc=13106&sc=13107&sc=13108&sc=13118&sc=13121&sc=13122&sc=13123&cb=0.0&ct=9999999&mb=0&mt=9999999&et=9999999&cn=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sngz=&po1=12&pc=50'
+# url_list.append('https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=13&sc=13101&sc=13102&sc=13103&cb=0.0&ct=9999999&mb=0&mt=9999999&et=9999999&cn=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sngz=&po1=12&pc=50')
+# url_list.append('https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=13&sc=13104&sc=13105&sc=13113&cb=0.0&ct=9999999&mb=0&mt=9999999&et=9999999&cn=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sngz=&po1=12&pc=50')
+# url_list.append('https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=13&sc=13106&sc=13107&sc=13108&cb=0.0&ct=9999999&mb=0&mt=9999999&et=9999999&cn=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sngz=&po1=12&pc=50')
+# url_list.append('https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=13&sc=13118&sc=13121&sc=13122&sc=13123&cb=0.0&ct=9999999&mb=0&mt=9999999&et=9999999&cn=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sngz=&po1=12&pc=50')
+# url_list.append('https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=13&sc=13109&sc=13110&cb=0.0&ct=9999999&mb=0&mt=9999999&et=9999999&cn=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sngz=&po1=12&pc=50')
+url_list.append('https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=13&sc=13111&sc=13112&cb=0.0&ct=9999999&mb=0&mt=9999999&et=9999999&cn=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sngz=&po1=12&pc=50')
+url_list.append('https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=13&sc=13114&sc=13115&cb=0.0&ct=9999999&mb=0&mt=9999999&et=9999999&cn=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sngz=&po1=12&pc=50')
+url_list.append('https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=13&sc=13120&cb=0.0&ct=9999999&mb=0&mt=9999999&et=9999999&cn=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sngz=&po1=12&pc=50')
+url_list.append('https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=13&sc=13116&sc=13117&cb=0.0&ct=9999999&mb=0&mt=9999999&et=9999999&cn=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sngz=&po1=12&pc=50')
+url_list.append('https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=13&sc=13119&cb=0.0&ct=9999999&mb=0&mt=9999999&et=9999999&cn=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sngz=&po1=12&pc=50')
 
-url_list.append(url_of_center)
-url_list.append(url_of_north)
-url_list.append(url_of_south)
-url_list.append(url_of_east)
-url_list.append(url_of_west)
-
-for url in url_list:
+for base_url in url_list:
 
 
     print("Request start --------------------")
-    result = requests.get(url)
+    headers={
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0'
+    }
+    result = requests.get(base_url,headers=headers)
 
     print("Generate object start ------------")
     c = result.content
@@ -53,10 +50,10 @@ for url in url_list:
 
     print("Make page list -------------------")
     urls = []
-    urls.append(url)
+    urls.append(base_url)
     for i in range(pages_split-1):
         pg = str(i+2)
-        url_page = url + '&pn=' + pg
+        url_page =base_url + '&pn=' + pg
         urls.append(url_page)
     print("  page list length: ",len(urls))
 
@@ -67,27 +64,30 @@ for url in url_list:
 
 
     print("Collect informations -------------")
-    name = [] #マンション名
-    address = [] #住所
-    locations0 = [] #立地1つ目（最寄駅/徒歩~分）
-    locations1 = [] #立地2つ目（最寄駅/徒歩~分）
-    locations2 = [] #立地3つ目（最寄駅/徒歩~分）
-    age = [] #築年数
-    height = [] #建物高さ
-    floor = [] #階
-    rent = [] #賃料
-    admin = [] #管理費
-    others = [] #敷/礼/保証/敷引,償却
-    floor_plan = [] #間取り
-    area = [] #専有面積
-
     progress = tqdm(total=hits)
 
     for url in urls:
+        name = [] #マンション名
+        address = [] #住所
+        locations0 = [] #立地1つ目（最寄駅/徒歩~分）
+        locations1 = [] #立地2つ目（最寄駅/徒歩~分）
+        locations2 = [] #立地3つ目（最寄駅/徒歩~分）
+        age = [] #築年数
+        height = [] #建物高さ
+        floor = [] #階
+        rent = [] #賃料
+        admin = [] #管理費
+        others = [] #敷/礼/保証/敷引,償却
+        floor_plan = [] #間取り
+        area = [] #専有面積
+
         result = requests.get(url)
         c = result.content
         soup = BeautifulSoup(c,"lxml")
         summary = soup.find("div",{'id':'js-bukkenList'})
+        if(isinstance(summary,type(None))):
+            print(url)
+            continue
 
         #マンション名、住所、立地（最寄駅/徒歩~分）、築年数、建物高さが入っているcassetteitemを全て抜き出し
         cassetteitems = summary.find_all("div",{'class':'cassetteitem'})
@@ -95,7 +95,6 @@ for url in url_list:
         for i in range(len(cassetteitems)):
             #各建物から売りに出ている部屋数を取得
             tbodies = cassetteitems[i].find_all('tbody')
-
             #マンション名取得
             subtitle = cassetteitems[i].find("div",{'class':'cassetteitem_content-title'})
             subtitle = subtitle.get_text()
@@ -167,32 +166,15 @@ for url in url_list:
                     area.append(data[index+5])
                 index +=1
             progress.update(len(tbodies))
-        time.sleep(3)
+        with open('suumo.csv', 'a') as f:
+            for i in range(len(name)):
+                line = ""
+                try:
+                    line = name[i]+","+address[i]+","+locations0[i]+","+locations1[i]+","+locations2[i]+","+age[i]+","+height[i]+","+floor[i]+","+rent[i]+","+admin[i]+","+others[i]+","+floor_plan[i]+","+area[i]+"\n"
+                except:
+                    continue
+                f.write(line)
+        time.sleep(random.randint(60,100))
 
     progress.close()
-
-
-    print("Data serialize --------------------")
-    name = Series(name)
-    address = Series(address)
-    locations0 = Series(locations0)
-    locations1 = Series(locations1)
-    locations2 = Series(locations2)
-    age = Series(age)
-    height = Series(height)
-    floor = Series(floor)
-    rent = Series(rent)
-    admin = Series(admin)
-    others = Series(others)
-    floor_plan = Series(floor_plan)
-    area = Series(area)
-
-    suumo_df = pd.concat([name, address, locations0, locations1, locations2, age, height, floor, rent, admin, others, floor_plan, area], axis=1)
-
-    suumo_df.columns=['マンション名','住所','立地1','立地2','立地3','築年数','建物高さ','階','賃料','管理費', '敷/礼/保証/敷引,償却','間取り','専有面積']
-
-    print(suumo_df)
-    suumo_df.to_csv('suumo.csv')
-
-
 
